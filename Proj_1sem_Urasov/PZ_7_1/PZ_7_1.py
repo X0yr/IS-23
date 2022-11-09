6a88d92e04c2f60bbcb046a787e7d95d1e246f58
sadplay.channel@gmail.com <sadplay.channel@gmail.com>
2022-11-09 23:51:49 +0300

    My work

--- Proj_1sem_Urasov/PZ_7_1/PZ_7_1.py
+++ Proj_1sem_Urasov/PZ_7_1/PZ_7_1.py
@@ -1,4 +1,26 @@
 import unittest
+# try:
+#     s=str(input("Введдите что-то >>>  "))
+#     s1=str(input("Введите ещё что-нибудь >>>  "))
+#     print(s.count(s1))
+# except ValueError as ve:
+#     print("")
+
+
+def colstr(s, s1):
+    a=len(s)
+    c=len(s1)
+    v=0
+    x=0
+    p=0
+    while v<=a:
+        if s[x:c] == s1:
+           p+=1
+        x+=1
+        c+=1
+        v+=1
+    print(p)
 
 s=str(input("Введдите что-то >>>  "))
 s1=str(input("Введите ещё что-нибудь >>>  "))
+colstr(s, s1)
