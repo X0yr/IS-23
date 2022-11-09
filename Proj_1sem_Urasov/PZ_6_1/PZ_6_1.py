20c7dbf1e533aadf49f28c648017ab4b3c343576
X0yr <sadplay.channel@gmail.com>
2022-11-09 14:38:16 +0300

    My work

--- Proj_1sem_Urasov/PZ_6_1/PZ_6_1.py
+++ Proj_1sem_Urasov/PZ_6_1/PZ_6_1.py
@@ -1,26 +1,26 @@
-import random
-def bbb(v):
+import random #подключение библиотеки
+def bbb(v): #Создание вункции
     a = []
-    for m in v:
-        if m % 2 == 1:
-            a.append(m)
-    return a
-try:
+    for m in v: #Создание цикла for
+        if m % 2 == 1: #условие
+            a.append(m) #добавление в список a числа m
+    return a #возвращает список a
+try: #обработчик исключений
     N=int(input('введите число >>> '))
     a=[]
     y=0
-    while y<N:
-        a.append(random.randint(1, 1000))
+    while y<N: #создание цикла while
+       a.append(random.randint(1, 1000)) #Добавление в список a рандомного числа от 1 до 1000
        y+=1
-    s=bbb(a)
-    j=len(s)
+    s=bbb(a) #присваевание переменной функции
+    j=len(s) #присваевание переменной значения равному количеству элементов в списке s
     p=0
-    if s != []:
-       for p in range(j):
-           s[p]+s[-1]
+    if s != []: #условие
+       for p in range(j): #Создание цикла for
+           s[p]+s[-1] #добавление к элементу под номером p последнего элемента списка
            p+=1
        print(s)
     else:
        print(a)
-except:
-    print('Error')
+except ValueError as ve:
+    print('Не целочисленное', ve)
---
+++ Proj_1sem_Urasov/PZ_7_1/PZ_7_1.py
@@ -0,0 +1,4 @@
+import unittest
+
+s=str(input("Введдите что-то >>>  "))
+s1=str(input("Введите ещё что-нибудь >>>  "))
