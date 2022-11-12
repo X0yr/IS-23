436f7e03f0c80af2b683d9af8912e70ae07c39b4
X0yr <sadplay.channel@gmail.com>
2022-11-12 18:58:40 +0300

    My work

--- Proj_1sem_Urasov/PZ_7_1/PZ_7_2.py
+++ Proj_1sem_Urasov/PZ_7_1/PZ_7_2.py
@@ -1,6 +1,13 @@
+import re
+
+
 stroca_1 = "Дана строка состоящая из русских слов разделенных пробелами"
-list_1 = stroca_1.split(" ")
-myList_1 = 0
-for i in range(len(list_1)):
-    if len(list_1[i]) < len(list_1[myList_1]):
-        print(list_1[i])
+print(min(re.findall(r'[а-яА-ЯёЁ]+', stroca_1)[::-1], key=len)) #с помошью функции re.findall перебираем все
+                                                                # слова с заданным алфавилом в стоке и выводим минимальное
+
+# list_1 = stroca_1.split(" ")
+# myList_1 = 0
+# for i in range(len(list_1)):
+#     if len(list_1[i]) <= len(list_1[myList_1]):
+#         print(list_1[i])
+#         break
