fe9cecf9f3c60f38e5ad209d0747e2b3da3b9a18
SystemFrozen <salijo@bk.ru>
2023-02-17 17:03:20 +0300

    my work

--- Proj_1sem_Urasov/Arkanoid/Game.py
+++ Proj_1sem_Urasov/Arkanoid/Game.py
@@ -1,14 +1,1 @@
-def sq_all(numbers):
-    for n in numbers:
-        yield n ** 2
-numbers = [6, 57, 4, 7, 68, 95]
-squares = sq_all(numbers)
-for i in squares:
-    print(i)
-# Вариант 2
-def sq_all(numbers): #оператор for убирается как самостоятельная конструкция
-    yield from [n ** 2 for n in numbers]
-numbers = [6, 57, 4, 7, 68, 95]
-squares = sq_all(numbers)
-for i in squares:
-    print(i)
+
